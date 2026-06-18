bca2d53a53331d3a15daacc1053b263bc7aadcbd
Olakunlevpn <olakunlevpn@live.com>
2026-06-18 14:42:18 +0100

    style: add top spacing to settings save button

--- resources/views/filament/clusters/settings/pages/abstract-settings-page.blade.php
+++ resources/views/filament/clusters/settings/pages/abstract-settings-page.blade.php
@@ -2,7 +2,7 @@
     <form wire:submit="save" class="space-y-6">
         {{ $this->form }}
 
-        <div class="flex justify-end">
+        <div style="margin-top: 2rem; padding-top: 1.5rem; border-top: 1px solid rgba(255,255,255,0.06); display: flex; justify-content: flex-end;">
             <x-filament::button type="submit" wire:loading.attr="disabled">
                 {{ __('cms.settings_cluster.save') }}
             </x-filament::button>
